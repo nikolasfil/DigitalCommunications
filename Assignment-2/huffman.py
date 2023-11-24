b4ca5b8467414f0b720392b3b3070a2db137f8ae
nikolasfil <nikolas13in@gmail.com>
2023-11-24 03:27:47 +0200

    updating the code

--- Assignment-2/huffman.py
+++ Assignment-2/huffman.py
@@ -1,5 +1,6 @@
-import math 
+import math
 from huffman_tree import *
+
 
 def huffman(output):
     # output: dictionary of output symbols and their probabilities
@@ -17,7 +18,7 @@ def huffman(output):
     for key in output:
         codes[key] = ""
     # create a huffman tree
-    
+
     while len(nodes) > 1:
         # sort nodes by probability
         nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
@@ -35,137 +36,147 @@ def huffman(output):
 
     return codes
 
+
 def print_I_info(output):
     result = "| I | Log |\n"
-    for key,value in output.items():
+    for key, value in output.items():
         # do -log2(value )
         result += f"| I<sub>{key}</sub> | {-1*math.log2(value)} |\n"
-        
+
     return result
 
 
 def print_codes(codes):
     result = "| Symbol | Code |\n"
-    for key,value in codes.items():
+    for key, value in codes.items():
         result += f"| {key} | {value} |\n"
     return result
 
 
 def change_to_letters(dictionary):
     result = {}
-    for i,value in enumerate(dictionary.values()):
+    for i, value in enumerate(dictionary.values()):
         result[f"a{i+1}"] = value
     return result
 
 
-def change_to_numbering(dictionary,letters):
+def change_to_numbering(dictionary, letters):
     result = {}
     letters = list(letters.keys())
-    for i,value in enumerate(dictionary.values()):
+    for i, value in enumerate(dictionary.values()):
         result[letters[i]] = value
     return result
 
 
 def change_to_single_letter(dictionary):
     result = {}
-    for i,value in enumerate(dictionary.values()):
+    for i, value in enumerate(dictionary.values()):
         result[f"{chr(97+i)}"] = value
     return result
 
 
 def print_H_info(output):
-    val = 0 
+    val = 0
     H = "$$\n H(x) = "
     for key in output.keys():
         # H+=f"I<sub>{key}</sub> * P<sub>{key}</sub> + "
-        H+="I_{"+f"{key}"+"} \cdot P_{"+f"{key}"+"} + "
+        H += "I_{" + f"{key}" + "} \cdot P_{" + f"{key}" + "} + "
     # H = H[:-2]
-    H+= " = "
+    H += " = "
     # round to 3 decimal places
-    for key,value in output.items():
-        
-        I = round(-1 *math.log2(value),3)
-        val += (I)*value
+    for key, value in output.items():
+        I = round(-1 * math.log2(value), 3)
+        val += (I) * value
         # H+=f"{I} * {value}+"
-        H+=f"{I} \cdot {value}+"
+        H += f"{I} \cdot {value}+"
 
         # H+=f"{(-1*math.log2(value))}*{value}+"
         # val += (-1*math.log2(value))*value
-    
+
     H = H[:-1]
     # round to 3 decimal places
-    val = round(val,3)
-    H+= f" = {val} \n$$ \n\nH(x) = {val}\n"
-    return H,val
+    val = round(val, 3)
+    H += f" = {val} \n$$ \n\nH(x) = {val}\n"
+    return H, val
 
 
-def print_R_info(codes,output):
-    val = 0 
+def print_R_info(codes, output):
+    val = 0
     R = "$$\n R(x) = "
-    for key,value in codes.items():
+    for key, value in codes.items():
         # R+=f"{output[key]} * {len(str(value))} + "
-        R+=f"{output[key]} \cdot {len(str(value))} + "
-        val += output[key]*len(str(value))
+        R += f"{output[key]} \cdot {len(str(value))} + "
+        val += output[key] * len(str(value))
     R = R[:-2]
-    val = round(val,3)
-    R+= f" = {val} \n$$ \n\nR(x) = {val}\n"
-    return R,val
+    val = round(val, 3)
+    R += f" = {val} \n$$ \n\nR(x) = {val}\n"
+    return R, val
 
 
-def print_n_info(r,h,pairs=1):
+def print_n_info(r, h, pairs=1):
     h = float(h)
     r = float(r)
-    val = round(pairs*h/r,3)
-    result ="$$\n n = \\frac{"+f"{pairs if pairs !=1 else ''}"+"H(x)}{R} = "+"\\frac{"+f"{pairs*h}"+"}{"+f"{r}"+"}"+f" = {val} = {val*100}\\ \% \n$$\n\n n = {val} " 
-    # result =f"n = H(x)/R = {h}/{r} = {val}" 
-    return result ,val
+    val = round(pairs * h / r, 3)
+    result = (
+        "$$\n n = \\frac{"
+        + f"{pairs if pairs !=1 else ''}"
+        + "H(x)}{R} = "
+        + "\\frac{"
+        + f"{pairs*h}"
+        + "}{"
+        + f"{r}"
+        + "}"
+        + f" = {val} = {val*100}\\ \% \n$$\n\n n = {val} "
+    )
+    # result =f"n = H(x)/R = {h}/{r} = {val}"
+    return result, val
 
 
 def get_combinations(output):
-    result_list = [] 
+    result_list = []
     for key_a in output.keys():
         for key_b in output.keys():
-            result_list.append((key_a,key_b))
+            result_list.append((key_a, key_b))
     result_dict = {}
     for key in result_list:
-        result_dict["".join(key)] = round(output[key[0]]*output[key[1]],3)
+        result_dict["".join(key)] = round(output[key[0]] * output[key[1]], 3)
 
     result = "\n\n| Combination | Probability |\n"
-    for key,value in result_dict.items():
-        result += f"| {key} | {value} |\n" 
-    return result,result_dict
+    for key, value in result_dict.items():
+        result += f"| {key} | {value} |\n"
+    return result, result_dict
 
 
-def get_combinations_different(output1,output2):
-    result_list = [] 
+def get_combinations_different(output1, output2):
+    result_list = []
     for key_a in output1.keys():
         for key_b in output2.keys():
-            result_list.append((key_a,key_b))
+            result_list.append((key_a, key_b))
     result_dict = {}
     for key in result_list:
-        result_dict["".join(key)] = round(output1[key[0]]*output2[key[1]],3)
+        result_dict["".join(key)] = round(output1[key[0]] * output2[key[1]], 3)
 
     result = "\n\n| Combination | Probability |\n"
-    for key,value in result_dict.items():
-        result += f"| {key} | {value} |\n" 
-    return result,result_dict
+    for key, value in result_dict.items():
+        result += f"| {key} | {value} |\n"
+    return result, result_dict
 
 
 def write_to_file(listing):
-    with open("code_result.md","w", encoding="UTF-8") as f:
+    with open("code_result.md", "w", encoding="UTF-8") as f:
         f.write(listing)
 
 
-def huffman_encode_pairs(combinations_dict,pairs=2):
+def huffman_encode_pairs(combinations_dict, pairs=2):
     """Returns the encoded huffman codes for the combinatios of x pairs"""
     result = ""
     huffman_encoded = huffman_encoding(combinations_dict)
-    huffman_pairs_encoded = {key:value for key,value in huffman_encoded.items() if len(key)==2*pairs}
+    huffman_pairs_encoded = {
+        key: value for key, value in huffman_encoded.items() if len(key) == 2 * pairs
+    }
     result = "\n\n| Combination | Code |\n"
     for key in sorted(huffman_pairs_encoded.keys()):
         temp_result = f"| {key} | {huffman_pairs_encoded[key]} | "
         result += temp_result + "\n"
 
-    return result,huffman_pairs_encoded
-
-
+    return result, huffman_pairs_encoded
--- Assignment-2/main_application.py
+++ Assignment-2/main_application.py
@@ -1,5 +1,5 @@
-from huffman import * 
-from huffman_tree import * 
+from huffman import *
+from huffman_tree import *
 from efficiency_plotter import *
 
 
@@ -7,10 +7,8 @@ def main():
     output = {"a": 0.53, "b": 0.37, "c": 0.10}
     codes = huffman(output)
 
-
     output = change_to_letters(output)
     codes = change_to_letters(codes)
-
 
     # Assignment 1
 
@@ -18,53 +16,77 @@ def main():
     pretty_codes = print_codes(codes)
     i_info = print_I_info(output)
     h_info, h_value = print_H_info(output)
-    r_info, r_value = print_R_info(codes,output)
-    n_info, n_value = print_n_info(r_value,h_value)
-    
+    r_info, r_value = print_R_info(codes, output)
+    n_info, n_value = print_n_info(r_value, h_value)
 
-    # ----------------------- Assignment 2 -------------------- 
+    # ----------------------- Assignment 2 --------------------
     assignment_2_header = "\n\n## Assignment-2\n\n"
 
-    combinations_info,combinations_dict = get_combinations(output)
+    combinations_info, combinations_dict = get_combinations(output)
 
+    huffman_encoded_info, huffman_encoded_value = huffman_encode_pairs(
+        combinations_dict
+    )
 
-    huffman_encoded_info, huffman_encoded_value = huffman_encode_pairs(combinations_dict)
+    huffman_r_info, huffman_r_value = print_R_info(
+        huffman_encoded_value, combinations_dict
+    )
 
+    huffman_h_info = (
+        "$$\n H(x) = 2 H(x) = 2 \cdot "
+        + f"{h_value} = {2*h_value} \n$$\n\nH(x) = {2*h_value}\n"
+    )
 
-    huffman_r_info, huffman_r_value = print_R_info(huffman_encoded_value,combinations_dict)
-    
-    huffman_h_info = "$$\n H(x) = 2 H(x) = 2 \cdot "+f"{h_value} = {2*h_value} \n$$\n\nH(x) = {2*h_value}\n"
+    huffman_n_info, huffman_n_value = print_n_info(huffman_r_value, h_value, 2)
 
-    huffman_n_info, huffman_n_value = print_n_info(huffman_r_value,h_value,2)
+    # assignment 2 - 3
 
-    # assignment 2 - 3 
+    combinations_3_pair_info, combinations_3_pair_dict = get_combinations_different(
+        output, combinations_dict
+    )
 
-    combinations_3_pair_info, combinations_3_pair_dict = get_combinations_different(output,combinations_dict)
-    
-    huff_encoded_3_pair_info, huff_encoded_3_pair_dict = huffman_encode_pairs(combinations_3_pair_dict,3)
+    huff_encoded_3_pair_info, huff_encoded_3_pair_dict = huffman_encode_pairs(
+        combinations_3_pair_dict, 3
+    )
 
-    huff_encoded_3_pair_r_info, huff_encoded_3_pair_r_dict = print_R_info(huff_encoded_3_pair_dict , combinations_3_pair_dict)
-    
-    huff_encoded_3_h_info = "$$\n H(x) = 3 H(x) = 2 \cdot "+f"{h_value} = {2*h_value} \n$$\n\nH(x) = {2*h_value}\n"
+    huff_encoded_3_pair_r_info, huff_encoded_3_pair_r_dict = print_R_info(
+        huff_encoded_3_pair_dict, combinations_3_pair_dict
+    )
 
-    huff_encoded_3_n_info , huff_encoded_3_n_value= print_n_info(huff_encoded_3_pair_r_dict,h_value,3)
+    huff_encoded_3_h_info = (
+        "$$\n H(x) = 3 H(x) = 2 \cdot "
+        + f"{h_value} = {2*h_value} \n$$\n\nH(x) = {2*h_value}\n"
+    )
 
-    image_plotted = plotter([n_value,huffman_n_value,huff_encoded_3_n_value])
-    print([n_value,huffman_n_value,huff_encoded_3_n_value])
+    huff_encoded_3_n_info, huff_encoded_3_n_value = print_n_info(
+        huff_encoded_3_pair_r_dict, h_value, 3
+    )
 
-    # ----------------------- General ----------------------- 
+    image_plotted = plotter([n_value, huffman_n_value, huff_encoded_3_n_value])
+    print([n_value, huffman_n_value, huff_encoded_3_n_value])
 
-    lst = [assignment_1_header,pretty_codes,i_info,h_info,r_info,n_info, 
-           assignment_2_header,combinations_info,
-           huffman_encoded_info,huffman_r_info,
-           huffman_h_info,huffman_n_info,
-            combinations_3_pair_info,
-            huff_encoded_3_pair_info,
-            huff_encoded_3_pair_r_info,
-            huff_encoded_3_h_info,
-            huff_encoded_3_n_info,
-            image_plotted
-           ]
+    # ----------------------- General -----------------------
+
+    lst = [
+        assignment_1_header,
+        pretty_codes,
+        i_info,
+        h_info,
+        r_info,
+        n_info,
+        assignment_2_header,
+        combinations_info,
+        huffman_encoded_info,
+        huffman_r_info,
+        huffman_h_info,
+        huffman_n_info,
+        combinations_3_pair_info,
+        huff_encoded_3_pair_info,
+        huff_encoded_3_pair_r_info,
+        huff_encoded_3_h_info,
+        huff_encoded_3_n_info,
+        image_plotted,
+    ]
     result = "\n\n".join(lst)
 
     # print(result)
