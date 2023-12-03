afbaf682a1b3dcd8d3084111944bb3bc1eaff2b7
nikolasfil <nikolas13in@gmail.com>
2023-12-03 23:22:39 +0200

    updating along the way

--- Assignment-4/assignment_4.py
+++ Assignment-4/assignment_4.py
@@ -13,13 +13,88 @@ def save_code():
     return lines
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Function definition
+def f1(t):
+    if 0 <= t < 1:
+        return 1 / np.sqrt(2)
+    elif 1 <= t < 2:
+        return -1 / np.sqrt(2)
+    else:
+        return 0
+
+
+def f2(t):
+    if 2 <= t <= 4:
+        return -1 / np.sqrt(2)
+    else:
+        return 0
+
+
+def plot_f1(t_values, file):
+    # Calculate corresponding f1 values
+    values = [f1(t) for t in t_values]
+
+    # Plot the function
+    plt.plot(t_values, values, label=r"$f_{1}(t)=\frac{S_{1}}{\sqrt{2}}$")
+
+    # Set x and y axis limits to start from 0
+    plt.xlim(0, 4)
+    plt.ylim(-1, 1)
+
+    # Add labels and title
+    plt.xlabel("t")
+    plt.ylabel(r"$f_{1}(t)$")
+    plt.title("Plot of $f_{1}(t)$ for $S_{1}=\pm1$")
+    plt.grid(True)
+    plt.legend()
+
+    # Show the plot
+    # plt.show()
+    plt.savefig(file)
+    plt.close()
+
+
+def plot_f2(t_values, file):
+    # Calculate corresponding f1 values
+    values = [f2(t) for t in t_values]
+
+    # Plot the function
+    plt.plot(t_values, values, label=r"$f_{2}(t)=\frac{S_{2}}{\sqrt{2}}$")
+
+    # Set x and y axis limits to start from 0
+    plt.xlim(0, 4)
+    plt.ylim(-1, 1)
+
+    # Add labels and title
+    plt.xlabel("t")
+    plt.ylabel(r"$f_{2}(t)$")
+    plt.title("Plot of $f_{2}(t)$ for $S_{2}=\pm1$")
+    plt.grid(True)
+    plt.legend()
+
+    # Show the plot
+    # plt.show()
+    plt.savefig(file)
+    plt.close()
+
+
 def main():
-    result = []
+    # Generate values for t
+    t_values = np.linspace(0, 5, 1000)
 
-    result.append("## Assignment-4\n\n")
-    result.append("\n\n")
+    # Plot the function
+    plot_f1(t_values, "assignment_4_f1.png")
+    plot_f2(t_values, "assignment_4_f2.png")
+    # result = []
 
-    print("".join(result))
+    # result.append("## Assignment-4\n\n")
+    # result.append("\n\n")
+
+    # print("".join(result))
 
     # Saving Code
 
@@ -27,7 +102,7 @@ def main():
     # result.append("".join(save_code()))
     # result.append("\n\n")
 
-    save_to_file(result, "../MD_Reports/assignment-4-code-result.md")
+    # save_to_file(result, "../MD_Reports/assignment-4-code-result.md")
 
 
 if __name__ == "__main__":
